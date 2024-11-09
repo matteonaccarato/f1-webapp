5528b01e91aaf7e5e77380f3a0da8dd504930a53
Matteo Naccarato <matteo.naccarato@itiscuneo.eu>
2024-11-09 23:08:10 +0100

    Fix drivers scraping

--- controllers/drivers/drivers.php
+++ controllers/drivers/drivers.php
@@ -3,6 +3,7 @@ if (!set_include_path("{$_SERVER['DOCUMENT_ROOT']}"))
     error("500", "set_include_path()");
 
 const BASE_URL = "https://www.formula1.com/en/drivers.html";
+const BASE_F1 = "https://www.formula1.com";
 
 function f1_scrape_drivers($base_url): array {
 
@@ -19,14 +20,14 @@ function f1_scrape_drivers($base_url): array {
     @$html->loadHtml($page);
     $xpath = new DOMXPath($html);
 
-    // Get TEAMS
+    // TEAMS
     $node_list = $xpath->query('//p[@class="f1-heading tracking-normal text-fs-12px leading-tight normal-case font-normal non-italic f1-heading__body font-formulaOne text-greyDark"]');
     foreach ($node_list as $n) {
         $team = $n->nodeValue;
         $team_list[] = $team;
     }
 
-    // Get NAMES
+    // NAMES
     $node_list = $xpath->query('//div[contains(@class, "f1-driver-name")]');
     foreach ($node_list as $n) {
         $name = "";
@@ -37,29 +38,28 @@ function f1_scrape_drivers($base_url): array {
         $name_list[] = $name;
     }
 
-    // Get FLAGS, NUMBERS, IMGs
-    $saving_idx = 0;
-    $node_list = $xpath->query('//div[contains(@class, "f1-inner-wrapper")]//img');
-    for ($i=0; $i<$node_list->count(); ++$i) {
-        if ($node_list->item($i)) {
-            $link = $node_list->item($i)->getAttribute("src");
-            switch ($saving_idx) {
-                case 0: $flag_list[] = $link; break;
-                case 1: $number_list[] = $link; break;
-                case 2: $img_list[] = $link; break;
-            }
-            $saving_idx++;
-            $saving_idx = $saving_idx % 3;
-        }
+    // FLAGS
+    $node_list = $xpath->query('//img[@class="f1-c-image h-[2em] ml-auto mr-0 border border-greyDark rounded-xxs"]');
+    foreach ($node_list as $node) {
+        $flag_list[] = $node->getAttribute("src");
+    }
+
+    // NUMBERS
+    $node_list = $xpath->query('//img[@class="f1-c-image f1-utils-square-block text-[6rem]"]');
+    foreach ($node_list as $node) {
+        $number_list[] = $node->getAttribute("src");
+    }
+
+    // IMGs
+    $node_list = $xpath->query('//img[@class="f1-c-image ml-0 mr-0 pr-s max-w-3/4"]');
+    foreach ($node_list as $node) {
+        $img_list[] = $node->getAttribute("src");
     }
 
     // Get EXTRA INFO
-    $node_list = $xpath->query('//a[contains(@class, "focus-visible:outline-2")]');
-    for ($i=0; $i<$node_list->count(); ++$i) {
-        if ($node_list->item($i)) {
-            $link = $node_list->item($i)->getAttribute("href");
-            $url_list[] = "https://www.formula1.com" . $link;
-        }
+    $node_list = $xpath->query('//a[@class="group focus-visible:outline-0"]');
+    foreach ($node_list as $node) {
+        $url_list[] = BASE_F1 . $node->getAttribute("href");
     }
 
     return [$name_list, $team_list, $flag_list, $number_list, $img_list, $url_list];
